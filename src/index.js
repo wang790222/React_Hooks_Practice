b66520268eb4584854d6d55f46f7adf3d0867a50
wang790222 <wang790222@gmail.com>
2019-08-20 12:02:50 -0400

    Get Resources

---
+++ src/index.js
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+import ReactDOM from 'react-dom';
+import ResourceList from './ResourceList';
+
+const App = () => {
+
+  const [resource, setResource] = useState('posts');
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => setResource('posts')}>Posts</button>
+        <button onClick={() => setResource('todos')}>Todos</button>
+      </div>
+      <ResourceList resource={resource} />
+    </div>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
